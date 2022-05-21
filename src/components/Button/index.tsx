0c0f1f08f77ad5168ac129a2deccb1d4464274f0
lauma.alves <lauma.alves@techtools.vc>
2022-05-21 14:04:58 -0300

    Salvando para formatar o pc

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './Button.module.scss'
+
+class Button extends React.Component {
+    render(){
+        return(
+            <button className={style.botao}>
+                botão
+            </button>
+        )
+    }
+}
+
+export default Button;
